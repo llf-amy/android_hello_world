ad520f9a1d21db648202d7b2cee1813d9b257dd8
Amy Wooding <amywooding@Amys-MacBook-Pro.local>
2015-07-10 11:29:03 -0700

    Initial commit

---
+++ app/src/androidTest/java/com/example/amywooding/myfirstapp/MyActivityTest.java
@@ -0,0 +1,10 @@
+package com.example.amywooding.myfirstapp;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by amywooding on 15-06-20.
+ */
+public class MyActivityTest extends TestCase {
+
+}
